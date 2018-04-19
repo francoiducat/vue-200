a09e1d3b23e59d91bf50c094b7f4712ec55aefbe
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-19 17:22:18 +0200

    step 7 done

--- src/views/router.js
+++ src/views/router.js
@@ -1,9 +1,8 @@
 import VueRouter from 'vue-router'
-
-//import Home from './Home.vue'
+import Home from './Home.vue'
 
 const routes = [
-
+  {name: "home", path: "/home", component: Home}
 ]
 
 export default new VueRouter({
