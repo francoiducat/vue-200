06e761f8118bcc7fbbb405e77073d0c51438cc7f
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-19 10:44:20 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/vue-material.css'
 
-Vue.use("VueMaterial")
+Vue.use(VueMaterial)
 
 new Vue({
   el: "#app",
