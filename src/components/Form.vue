22e687837f1ef1ac202cfea7502605cfc3af44fc
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-20 15:12:04 +0200

    step 14 WIP

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,79 +1,100 @@
 <template>
-    <md-card class="card-panel">
-        <md-card-header v-if="editMode">
-            <div class="md-title">Update {{ person.firstname }} {{ person.lastname }}</div>
-            <div class="md-subhead">{{person.id}}</div>
-        </md-card-header>        
-        <md-card-content>
-            <img class="picture" :src="person.photo || 'https://randomuser.me/api/portraits/lego/6.jpg'" />
-            <md-input-container>
-                <label>Firstname</label>
-                <md-input name="firstname" v-model="person.firstname"></md-input>
-            </md-input-container>
-            <md-input-container>
-                <label>Lastname</label>
-                <md-input name="lastname" v-model="person.lastname"></md-input>
-            </md-input-container>
-            <md-input-container>
-                <label>Email</label>
-                <md-input name="email" v-model="person.email"></md-input>
-            </md-input-container>
-            <md-input-container>
-                <label>Phone</label>
-                <md-input name="phone" v-model="person.phone"></md-input>
-            </md-input-container>
-            </form>
-        </md-card-content>
-        <md-card-actions>
-            <md-button @click.native="cancel">Cancel</md-button>
-            <md-button @click.native="submit">Submit</md-button>
-        </md-card-actions>
-    </md-card>
+  <md-card class="card-panel">
+    <md-card-header v-if="editMode">
+      <div class="md-title">Update {{ person.firstname }} {{ person.lastname }}</div>
+      <div class="md-subhead">{{person.id}}</div>
+    </md-card-header>
+    <md-card-content>
+      <img class="picture" :src="person.photo || 'https://randomuser.me/api/portraits/lego/6.jpg'"/>
+      <md-input-container :class="{}">
+        <label>Firstname</label>
+        <md-input @input="$v.person.firstname.$touch()"
+                  name="firstname"
+                  v-model="person.firstname"></md-input>
+        <span v-show="!$v.person.firstname.required">Champs obligatoire</span>
+      </md-input-container>
+      <md-input-container>
+        <label>Lastname</label>
+        <md-input
+          name="lastname"
+          v-model="person.lastname"></md-input>
+      </md-input-container>
+      <md-input-container>
+        <label>Email</label>
+        <md-input name="email" v-model="person.email"></md-input>
+      </md-input-container>
+      <md-input-container>
+        <label>Phone</label>
+        <md-input name="phone" v-model="person.phone"></md-input>
+      </md-input-container>
+      </form>
+    </md-card-content>
+    <md-card-actions>
+      <md-button @click.native="cancel">Cancel</md-button>
+      <md-button @click.native="submit">Submit</md-button>
+    </md-card-actions>
+  </md-card>
 
 </template>
 <script>
-    export default {
-        props: {
-            person:{
-                type:Object,
-                default:function(){
-                    return {firstname:'',lastname:'',email:'',phone:''};
-                }
-            }
-        },
-        data() {
-            return {
-                
-            }
-        },
-        computed: {
-            editMode: function () {
-                return this.person && this.person.id;
-            }
-        },
-        methods: {
-            submit: function () {
-                this.$emit('save', this.person);
-            },
-            cancel: function () {
-                this.$emit('cancel');
-            }
+  import {required, minLength, email} from 'vuelidate/lib/validators'
+
+  export default {
+    props: {
+      person: {
+        type: Object,
+        default: function () {
+          return {firstname: '', lastname: '', email: '', phone: ''};
         }
+      }
+    },
+    data () {
+      return {}
+    },
+    validations: {
+      person: {
+        firstname: {
+          required,
+          minLength: minLength(2)
+        },
+        lastname: {
+          required,
+          minLength: minLength(2)
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
+    computed: {
+      editMode: function () {
+        return this.person && this.person.id;
+      }
+    },
+    methods: {
+      submit: function () {
+        this.$emit('save', this.person);
+      },
+      cancel: function () {
+        this.$emit('cancel');
+      }
     }
+  }
 </script>
 <style scoped>
-    .card-panel {
-        padding: 10px;
-        margin: 10px !important;
-        min-width: 500px;
-    }
-    
-    .card {
-        margin: 0;
-    }
-    
-    .picture {
-        border-radius: 50%;
-        width: 80px;
-    }
+  .card-panel {
+    padding: 10px;
+    margin: 10px !important;
+    min-width: 500px;
+  }
+
+  .card {
+    margin: 0;
+  }
+
+  .picture {
+    border-radius: 50%;
+    width: 80px;
+  }
 </style>
