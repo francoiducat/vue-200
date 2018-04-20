e71130c5f155304764fff30a4d1ba20826109156
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-20 10:05:10 +0200

    step 10 done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <section class="container">    
-    <sfeir-card :person="person"></sfeir-card>
+  <section class="container">
+    <sfeir-card :person="person"
+    @personDelete="random"></sfeir-card>
     <md-button class="md-fab md-fab-bottom-right md-primary" @click="random">
       <md-icon>cached</md-icon>
     </md-button>
@@ -12,7 +13,7 @@ import CardPanel from '../components/CardPanel.vue'
 import peopleService from '../services/PeopleService.js';
 
 
-export default {  
+export default {
   components:{
     'sfeir-card': CardPanel
   },
@@ -52,80 +53,80 @@ export default {
 		margin: 10px !important;
 		min-width: 500px;
 	}
-	
+
 	.card-panel:hover {
 		box-shadow: 0 11px 15px -7px rgba(0, 0, 0, .2), 0 24px 38px 3px rgba(0, 0, 0, .14), 0 9px 46px 8px rgba(0, 0, 0, .12);
 	}
-	
+
 	.md-card .md-subhead {
 		opacity: 1;
 	}
-	
+
 	.picture {
 		border-radius: 50%;
 		width: 100%;
 		height: 100%;
 	}
-	
+
 	.username {
 		color: #337ab7;
 		font-size: 24px;
 		font-weight: 400;
 	}
-	
+
 	.lastname {
 		text-transform: uppercase;
 	}
-	
+
 	.subtitle {
 		color: rgba(0, 0, 0, 0.54);
 	}
-	
+
 	.people-data {
 		margin-top: 20px;
 	}
-	
+
 	.item {
 		color: #337ab7;
 	}
-	
+
 	.people-data a {
 		padding-left: 10px;
 	}
-	
+
 	.icon {
 		color: lightslategrey;
 		width: 24px;
 		height: 24px;
 	}
-	
+
 	.label {
 		font-weight: bold;
 	}
-	
+
 	.skills {
 		padding: 10px;
 		margin: 10px;
 		background-color: #FAFAFA;
 	}
-	
+
 	.skills h3 {
 		font-size: 24px;
 		font-weight: normal;
 		line-height: 1.1;
 	}
-	
+
 	.skills a {
 		background-color: white;
 		color: #000;
 		margin: 5px;
 	}
-	
+
 	.links {
 		text-align: center;
 	}
-	
+
 	.links img {
 		padding: 0 5px;
-	} 
+	}
 </style>
