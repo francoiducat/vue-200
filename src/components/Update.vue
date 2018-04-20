0a83b69655b524766b770edf809d4c8f47ca21cc
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-20 11:30:22 +0200

    step 12 WIP

---
+++ src/components/Update.vue
@@ -0,0 +1,18 @@
+<template>
+  <sfeir-form  ></sfeir-form>
+</template>
+
+<script>
+  import Form from './Form.vue'
+
+  export default {
+    name: "Update",
+    components: {
+      sfeirFrom: Form
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
